1aa9a46fd860724e2863a31bc08594ac38281e65
Faizal Abdurachman <pomahguesthouse@gmail.com>
2026-07-06 16:37:21 +0700

    Use package exports in XYFlow shim

--- src/lib/xyflow-react-shim.ts
+++ src/lib/xyflow-react-shim.ts
@@ -1,2 +1,2 @@
-export * from "@xyflow/react/dist/esm/index.js";
-export { ReactFlow as default } from "@xyflow/react/dist/esm/index.js";
+export * from "@xyflow/react";
+export { ReactFlow as default } from "@xyflow/react";
